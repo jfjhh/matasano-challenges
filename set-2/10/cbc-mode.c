94f7685329e63a49e7f32e159266ea739c4bc67b
jfjhh <alex.striff1@gmail.com>
2015-10-31 12:52:26 -0700

    Implemented AES in CBC mode

--- set-2/10/cbc-mode.c
+++ set-2/10/cbc-mode.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <openssl/aes.h>
 
 /* Index within a block. */
 #define BLOCK(A)	((A >= BLOCKSIZE) ? (A % BLOCKSIZE) : A)
 
 static const size_t BLOCKSIZE = 16;
-
-static const unsigned char PASSKEY[] = { /* "YELLOW SUBMARINE". */
-	0x59, 0x45, 0x4c, 0x4c, 0x4f, 0x57, 0x20, 0x53,
-	0x55, 0x42, 0x4d, 0x41, 0x52, 0x49, 0x4e, 0x45,
-};
 
 static const unsigned char IV[] = { /* Initialization Vector (all 0x0). */
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -28,54 +24,68 @@ static inline void xorbuf(unsigned const char *restrict in,
 
 int main(int argc, const char *argv[])
 {
-	int i, j, c, encrypt;
-	unsigned char c_buf[BLOCKSIZE], p_buf[BLOCKSIZE], prev_buf[BLOCKSIZE];
-	FILE *file = NULL;
 	AES_KEY key;
+	FILE *file;
+	int i, j, c, encrypt;
+	unsigned char in_buf[BLOCKSIZE],   out_buf[BLOCKSIZE],
+				  prev_buf[BLOCKSIZE], passkey[BLOCKSIZE];
 
-	if (argc != 3 || !(file = fopen(argv[1], "rb"))) {
-		fputs("Usage: ./ecb <filename> <encrypt?>\n", stderr);
+	if (argc != 4) {
+		fputs("Usage: ./cbc-mode <'e'|'d'> <key> <filename|'-'>\n", stderr);
 		return 1;
+	} else if (argv[3][0] == '-') {
+		file = stdin;
+	} else if (!(file = fopen(argv[3], "rb"))) {
+		fprintf(stderr, "Could not open file '%s'.\n", argv[3]);
+		return 2;
 	}
-	encrypt = (argv[2][0] == 'e') ? 1 : 0;
 
-	/* IDK what I'm doing, zero all the things. */
-	memset(c_buf,    '\0', BLOCKSIZE);
-	memset(p_buf,    '\0', BLOCKSIZE);
-	memset(prev_buf, '\0', BLOCKSIZE);
-	i = j = c = 0;
+	memset(passkey, '\0',    BLOCKSIZE);
+	memcpy(passkey, argv[2], BLOCKSIZE);
+
+	encrypt = (argv[1][0] == 'e') ? 1 : 0;
 
 	if (encrypt)
-		AES_set_encrypt_key(PASSKEY, 128, &key);
+		AES_set_encrypt_key(passkey, 128, &key);
 	else
-		AES_set_decrypt_key(PASSKEY, 128, &key);
+		AES_set_decrypt_key(passkey, 128, &key);
 
-	i = 0;
-	while ((c = fgetc(file)) != EOF) { // Get the data.
-		if (c == '\n') /* Ignore line breaks in data. */
-			continue;
+	for (i = 0; (c = fgetc(file)) != EOF; i++) { /* Get the data. */
+		in_buf[BLOCK(i)] = c;
+		fprintf(stderr, "%02d:\t0x%02x  %c\n",
+				i, c, (isprint(c) ? c : '\0'));
 
-		c_buf[BLOCK(i)] = c;
-		fputc(c_buf[BLOCK(i)], stderr);
+		if (i % BLOCKSIZE == (BLOCKSIZE - 1) && i > 0) {
+			fprintf(stderr, "%scrypting, on byte %d.\n",
+					(encrypt ? "En" : "De"), i);
 
-		// if (i == (BLOCKSIZE - 1) || i % (BLOCKSIZE - 1) == 0) {
-		// 	if (i == BLOCKSIZE - 1)
-		// 		memcpy(prev_buf, IV,    BLOCKSIZE);
-		// 	else
-		// 		memcpy(prev_buf, c_buf, BLOCKSIZE);
+			if (i == BLOCKSIZE - 1) /* Setup XOR block to chain with. */
+				memcpy(prev_buf, IV,      BLOCKSIZE);
+			/* else if (encrypt) */
+			/* 	memcpy(prev_buf, out_buf, BLOCKSIZE); */
 
-		// 	if (encrypt) {
-		// 		xorbuf(prev_buf,    p_buf,  BLOCKSIZE);
-		// 		AES_encrypt(p_buf,  c_buf,  &key);
-		// 	} else {
-		// 		AES_decrypt(c_buf,  p_buf,  &key);
-		// 		xorbuf(prev_buf,    p_buf,  BLOCKSIZE);
-		// 	}
+			fputs("\tPrevious buffer:\n\t", stderr);
+			for (j = 0; j < BLOCKSIZE; j++)
+				fprintf(stderr, "0x%02x ", prev_buf[j]);
+			fputs("\n\n", stderr);
 
-		// 	for (j = 0; j < BLOCKSIZE; j++)
-		// 		fputc(p_buf[j], stdout);
-		// }
-		i++;
+			if (encrypt) { /* Encrypt or Decrypt the block. */
+				xorbuf(prev_buf,    in_buf,  BLOCKSIZE);
+				AES_encrypt(in_buf, out_buf, &key);
+
+				/* Prepare for next block. */
+				memcpy(prev_buf, out_buf, BLOCKSIZE);
+			} else {
+				AES_decrypt(in_buf, out_buf,  &key);
+				xorbuf(prev_buf,    out_buf,  BLOCKSIZE);
+
+				/* Prepare for next block. */
+				memcpy(prev_buf, in_buf,  BLOCKSIZE);
+			}
+
+			for (j = 0; j < BLOCKSIZE; j++) /* Output block to stdout. */
+				fputc(out_buf[j], stdout);
+		}
 	}
 
 	fclose(file);
